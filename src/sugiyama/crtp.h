6e175834b3b8ffabff2533879101cd558dd6035c
Misaki Sugiyama <misaki-sugiyama@users.noreply.github.com>
2020-07-04 16:07:08 +0800

    Change CRTP helper to be a much simpler implementation with macro

--- src/sugiyama/crtp.h
+++ src/sugiyama/crtp.h
@@ -1,19 +1,6 @@
-// A helper class to reduce CRTP boilerplate (static_cast)
+// Some helper macros to reduce CRTP boilerplate (static_cast)
 #pragma once
 
-namespace sugiyama {
-
-  template <template<typename, typename...> class TypeCRTP, class Derived, typename... Ts>
-  class CRTPHelper {
-  private:
-    // Put everything in private for safe-guarding
-    Derived& derived() {
-      return static_cast<Derived&>(*this);
-    }
-    const Derived& derived() const {
-      return static_cast<const Derived&>(*this);
-    }
-    friend TypeCRTP<Derived, Ts...>;
-  };
-
-}
+// Just some small convenient syntactic sugars
+#define DERIVED (static_cast<Derived&>(*this))
+#define DERIVEDC (static_cast<const Derived&>(*this))
